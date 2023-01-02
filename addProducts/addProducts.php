1b975290b784bf1d317448e56d42f18c671368a0
Marouan Ajtirah <marouan_39@hotmail.com>
2023-01-02 22:37:25 +0100

    filter

--- addProducts/addProducts.php
+++ addProducts/addProducts.php
@@ -1,68 +1,97 @@
 <style>
+  body {
+    background: #161616;
+    font-family: sans-serif;
 
-body {
-  background: #161616;
-
-  font-family: sans-serif;
-
-
-  
-}
+  }
 
 
-
-form{
-padding-top: 100px;
+  form {
+    padding-top: 100px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    
 
-}
+
+
+  }
+
+  input {
+    padding: 5px;
+    width: 300px;
+    color: #161616;
+  }
+
+  h1 {
+    color: white;
+    display: flex;
+    justify-content: center;
+    position: relative;
+    top: 80px;
+  }
+
+  button {
+    padding: 10px;
+    width: 100px;
+    margin-top: 10px;
+    font-weight: 800;
+  }
 </style>
 
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Artikels toevoegen</title>
 </head>
+
 <body>
-    
 
 
+  <h1>Artikels toevoegen</h1>
 
-<form action="/addProducts/connection.php" method="POST">
-
-
-<input type="text" name = "title" placeholder="title">
-<br>
-<input type="text" name = "description" placeholder="description">
-<br>
-<input type="text" name = "createdAt" placeholder="createdAt">
-<br>
-<input type="text" name = "price" placeholder="price">
-<br>
-<input type="text" name = "active" placeholder="active">
-<br>
-<input type="text" name = "category_id" placeholder="category">
-<br>
-<input type="text" name = "img" placeholder="img">
-
-<button type="submit" name="submit"> Sign up</button>
+  <form action="/addProducts/connection.php" method="POST">
 
 
-</form>
+    <input type="text" name="title" placeholder="Title">
+    <br>
+    <input type="text" name="description" placeholder="Description">
+    <br>
+    <input type="text" name="createdAt" placeholder="Created at">
+    <br>
+    <input type="text" name="price" placeholder="Price">
+    <br>
+    <input type="text" name="active" placeholder="Active">
+    <br>
+    <input type="text" name="category_id" placeholder="Category">
+
+    <!-- <label for="category_id">Choose a pet:</label>
+    <select id="category_id">
+    <option value="">--Please choose an option--</option>
+    <option value="1">Premier League</option>
+    <option value="2">La Liga</option>
+    <option value="hamster">Hamster</option>
+    <option value="parrot">Parrot</option>
+    <option value="spider">Spider</option>
+    <option value="goldfish">Goldfish</option>
+</select> -->
+
+
+    <br>
+    <input type="text" name="img" placeholder="Img">
+
+    <button type="submit" name="submit"> Sign up</button>
+
+
+  </form>
 
 
 </body>
+
 </html>
-
-
-
-
--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+$title = "<h1>een <span>titel</span></h1>";
+//we requiren de json file. (bekijk hem eens. hij bevat de output van de door vite gegenereerde bestanden)
+$manifest = file_get_contents("./dist/manifest.json");
+//we lezen hem in als associatieve array
+$manifestObject = json_decode($manifest, true);
+?>
+
+<?php
 $db_host = '127.0.0.1';
 $db_user = 'root';
 $db_password = 'root';
@@ -33,7 +41,7 @@ if ($mysqli->connect_error) {
 
 // }
 
-$sql = "SELECT id,title,price,img FROM product";
+$sql = "SELECT id,title,price,img,category_id FROM product";
 $result = $mysqli->query($sql);
 
 $product = [];
@@ -50,53 +58,56 @@ $mysqli->close();
 
 <style>
   * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-/* --------------------- NAVBAR -------------------- */
+  /* --------------------- NAVBAR -------------------- */
 
-nav {
-  display: flex;
-  justify-content: space-around;
-  /* background-color: green; */
-  align-items: center;
-  padding: 10px;
-  max-width: 100%;
-  min-width: 1000px;
-}
-nav img {
-  height: 100px;
-  width: 150px;
-}
-nav ul {
-  display: flex;
-  justify-content: flex-end;
-  list-style: none;
-  gap: 30px;
-  margin-right: 30px;
-}
+  nav {
+    display: flex;
+    justify-content: space-around;
+    /* background-color: green; */
+    align-items: center;
+    padding: 10px;
+    max-width: 100%;
+    min-width: 1000px;
+  }
 
-nav ul li {
-  color: white;
-  text-decoration: none;
-}
-nav ul li:hover {
-  color: white;
-  text-decoration: underline;
-}
+  nav img {
+    height: 100px;
+    width: 150px;
+  }
 
-/* --------------------- BODY / MAIN -------------------- */
+  nav ul {
+    display: flex;
+    justify-content: flex-end;
+    list-style: none;
+    gap: 30px;
+    margin-right: 30px;
+  }
 
-body {
-  background: #161616;
-  color: white;
-  font-weight: 400;
-  font-family: sans-serif;
-  margin-top: 10px;
+  nav ul li {
+    color: white;
+    text-decoration: none;
+  }
 
-  /* background: rgb(3, 119, 149);
+  nav ul li:hover {
+    color: white;
+    text-decoration: underline;
+  }
+
+  /* --------------------- BODY / MAIN -------------------- */
+
+  body {
+    background: #161616;
+    color: white;
+    font-weight: 400;
+    font-family: sans-serif;
+    margin-top: 10px;
+
+    /* background: rgb(3, 119, 149);
   background: linear-gradient(
     90deg,
     rgba(3, 119, 149, 1) 0%,
@@ -108,165 +119,181 @@ body {
   color: white;
   font-weight: 400;
   font-family: sans-serif; */
-}
-main {
-  width: 100%;
-  position: relative;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  flex-direction: column;
-  padding: 0 8%;
+  }
+
+  main {
+    width: 100%;
+    position: relative;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: column;
+    padding: 0 8%;
+  }
+
+  .title {
+    font-size: 90px;
+    margin: 4rem 0 2rem 0;
+  }
+
+  .filters-wrapper {
+    width: 95%;
+    margin: 0 auto;
+  }
+
+  .filters-wrapper ul {
+    display: flex;
+    gap: 80px;
+    margin-bottom: 30px;
+    list-style: none;
+  }
+
+  .filters-wrapper ul li:hover {
+    text-decoration: underline;
+  }
+
+  .filters-wrapper ul li {
+    display: flex;
+    justify-content: space-between;
+    list-style: none;
+  }
+
+  .search-container {
+    margin: 1em 0;
+  }
+
+  .search-container input {
+    background-color: transparent;
+    width: 40%;
+    border-bottom: 2px solid #110f29;
+    padding: 1em 0.3em;
+    color: white;
+  }
+
+  .search-container input:focus {
+    border-bottom-color: #6759ff;
+  }
+
+  .search-container button {
+    padding: 1em 2em;
+    margin-left: 1em;
+    background-color: #6759ff;
+    color: white;
+    border-radius: 5px;
+    margin-top: 0.5em;
+  }
+
+  .button-value {
+    border: 2px solid #6759ff;
+    padding: 1em 2.2em;
+    border-radius: 3em;
+    background-color: transparent;
+    color: #6759ff;
+    cursor: pointer;
+  }
+
+  .button-value:hover {
+    background: linear-gradient(#6759ff, #110f29);
+  }
+
+  /* --------------------- PRODUCTS -------------------- */
+
+  .product-grid {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    overflow: hidden;
+    flex-wrap: wrap;
+  }
+
+  .product-container {
+    width: 23%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    flex-direction: column;
+    margin-bottom: 5rem;
+    display: none;
+  }
+  .show {
+  display: block;
 }
 
-.title {
-  font-size: 90px;
-  margin: 4rem 0 2rem 0;
-}
+  .product-img {
+    height: 300px;
+    width: 100%;
+    overflow: hidden;
+  }
 
-.filters-wrapper {
-  width: 95%;
-  margin: 0 auto;
-}
+  .product-img img {
+    width: 100%;
+  }
 
-.filters-wrapper ul {
-  display: flex;
-  gap: 80px;
-  margin-bottom: 30px;
-  list-style: none;
-}
-.filters-wrapper ul li:hover {
-  text-decoration: underline;
-}
-.filters-wrapper ul li {
-  display: flex;
-  justify-content: space-between;
-  list-style: none;
-}
-
-.search-container {
-  margin: 1em 0;
-}
-
-.search-container input {
-  background-color: transparent;
-  width: 40%;
-  border-bottom: 2px solid #110f29;
-  padding: 1em 0.3em;
-  color: white;
-}
-.search-container input:focus {
-  border-bottom-color: #6759ff;
-}
-
-.search-container button {
-  padding: 1em 2em;
-  margin-left: 1em;
-  background-color: #6759ff;
-  color: white;
-  border-radius: 5px;
-  margin-top: 0.5em;
-}
-
-.button-value {
-  border: 2px solid #6759ff;
-  padding: 1em 2.2em;
-  border-radius: 3em;
-  background-color: transparent;
-  color: #6759ff;
-  cursor: pointer;
-}
-.button-value:hover {
-  background: linear-gradient(#6759ff, #110f29);
-}
-
-/* --------------------- PRODUCTS -------------------- */
-
-.product-grid {
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  overflow: hidden;
-  flex-wrap: wrap;
-}
-
-.product-container {
-  width: 23%;
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  flex-direction: column;
-  margin-bottom: 5rem;
-}
-
-.product-img {
-  height: 300px;
-  width: 100%;
-  overflow: hidden;
-}
-
-.product-img img {
-  width: 100%;
-}
-/* .product-img:hover {
+  /* .product-img:hover {
   opacity: 0.6;
   transform: scale(1.1);
 } */
 
-.product-info {
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-  flex-direction: column;
-  /* margin-top: 1rem; */
-}
-
-.product-seller {
-  text-transform: uppercase;
-  font-size: 12px;
-  color: #999;
-}
-
-.product-title {
-  font-size: 20px;
-  margin: 5px 0;
-  font-weight: 500;
-}
-
-.product-price {
-  color: white;
-  font-size: 16px;
-  font-weight: 600;
-}
-
-/* --------------------- PRODUCTS ANIMATIONS -------------------- */
-
-.product-img img:hover {
-  width: 100%;
-  height: 100%;
-  animation: rotation 2s infinite linear;
-  /* opacity: 0.6; */
-  transform: scale(1.1);
-}
-
-@keyframes rotation {
-  from {
-    transform: rotateY(0deg);
+  .product-info {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-direction: column;
+    /* margin-top: 1rem; */
   }
-  to {
-    transform: rotateY(359deg);
+
+  .product-seller {
+    text-transform: uppercase;
+    font-size: 12px;
+    color: #999;
   }
-}
 
-section{
-  display: flex;
-  gap: 20px;
-}
+  .product-title {
+    font-size: 20px;
+    margin: 5px 0;
+    font-weight: 500;
+  }
 
-.filter-buttons{
-  padding-bottom: 30px;
-}
+  .product-price {
+    color: white;
+    font-size: 16px;
+    font-weight: 600;
+  }
+
+  /* --------------------- PRODUCTS ANIMATIONS -------------------- */
+
+  .product-img img:hover {
+    width: 100%;
+    height: 100%;
+    animation: rotation 2s infinite linear;
+    /* opacity: 0.6; */
+    transform: scale(1.1);
+  }
+
+  @keyframes rotation {
+    from {
+      transform: rotateY(0deg);
+    }
+
+    to {
+      transform: rotateY(359deg);
+    }
+  }
+
+  section {
+    display: flex;
+    gap: 20px;
+  }
+
+  .filter-buttons {
+    padding-bottom: 30px;
+  }
+
+  nav img {
+    border-radius: 4em;
+  }
+
 
 
 </style>
@@ -287,59 +314,63 @@ section{
     <a href="../Homepage/index.html"><img src="/images/logo.png" alt="logo" /></a>
     <ul>
       <li>
-        <a href="../Homepage/index.html"><i class="bi bi-house-door"></i></a>
+        <a href="../index.html"><i class="bi bi-house-door"></i></a>
       </li>
       <li><i class="bi bi-cart3"></i></li>
     </ul>
   </nav>
 
 
+
   <main>
     <h1 class="title">Football Shirts</h1>
     <div class="filters-wrapper">
-      <!-- <button>Premier league</button>
-        <button>La liga</button>
-        <button>Bundesliga</button>
-        <button>Serie A</button>
-        <button>Ligue 1</button> -->
       <div class="search-container">
         <input type="search" id="search-input" placeholder="Search product name here..." />
         <button id="search">Search</button>
       </div>
-      <div class="filter-buttons">
-        <button class="button-value">Premier league</button>
-        <button class="button-value">La Liga</button>
-        <button class="button-value">Bundesliga</button>
-        <button class="button-value">Serie A</button>
-        <button class="button-value"> Ligue 1</button>
+      <div id="filter-buttons">     
+        <button class="button-value active" onclick="filterSelection('all')"> Show all</button>
+        <button class="button-value" onclick="filterSelection('1')">Premier league</button>
+        <button class="button-value" onclick="filterSelection('2')">La Liga</button>
+        <button class="button-value" onclick="filterSelection('3')">Bundesliga</button>
+        <button class="button-value" onclick="filterSelection('4')">Serie A</button>
+        <button class="button-value" onclick="filterSelection('5')"> Ligue 1</button>
       </div>
     </div>
 
+ 
+
+
+ 
 
     <section>
-    <?php
-    $counter = 1;
-    foreach ($product as $key => $produkt) {
-    ?>
+      <?php
+      $counter = 1;
+      foreach ($product as $key => $produkt) {
+      ?>
 
 
-      <div class="product-container">
-        <div class="product-img">
-          <img src="/images/<?= $produkt["img"] ?>" alt="Product" />
+        <div class="product-container <?= $produkt["category_id"] ?>">
+          <div class="product-img">
+            <img src="/images/<?= $produkt["img"] ?>" alt="Product" />
+          </div>
+          <div class="product-info">
+            <span class="product-seller">Our Shop Name</span>
+            <h3 class="product-title"><?= $produkt["title"] ?> </h3>
+            <!-- <h3 class="product-description"><?= $produkt["description"] ?></h3> -->
+            <h3 class="product-price"><?= $produkt["price"] ?></h3>
+          </div>
         </div>
-        <div class="product-info">
-          <span class="product-seller">Our Shop Name</span>
-          <h3 class="product-title"><?= $produkt["title"] ?> </h3>
-          <!-- <h3 class="product-description"><?= $produkt["description"] ?></h3> -->
-          <h3 class="product-price"><?= $produkt["price"] ?></h3>
-        </div>
-      </div>
 
 
-    <?php
-      $counter++;
-    }
-    ?>
 
 
-</section>
+      <?php
+        $counter++;
+      }
+      ?>
+
+      <script src="./js/filter.js" defer></script>
+
+    </section>
